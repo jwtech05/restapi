dab8580e5f7f7b9dd03f6a7a80952e12d2949280
takar <takartin@gmail.com>
2021-07-13 16:44:54 +0900

    fix

--- restapi/settings.py
+++ restapi/settings.py
@@ -25,35 +25,11 @@ SECRET_KEY = 'django-insecure-!u=+ln^ak337+ajagrb&0biug2^4fw80z9m7%)fv3ajyvq(za9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
+ALLOWED_HOSTS = ['*' , '0.0.0.0']
 
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'profileapp',
-    'rest_framework',
-    'knox',
-
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'restapi.urls'
 
 TEMPLATES = [
     {
